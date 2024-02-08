6ac8ac76923688924cf4baa260279b0777b56b2d
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-02-08 15:43:17 +0100

    Added tests for filter chain

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,5 @@
-from filter_functions import filter_dot_folders, filter_files_only, filter_folder_only
+from filter_functions import (
+    filter_dot_folders, filter_files_only, filter_folder_only, exec_filter_chain)
 
 FILES_TO_FILTER = [
     ('2023-11-01', '11:32', '<DIR>', 'NT AUTHORITY\SYSTEM', '.', r'c:\\'),  # noqa: W605, E501
@@ -31,3 +32,66 @@ def test_files_only():
 def test_folders_only():
     lst = filter_folder_only(FILES_TO_FILTER)
     assert len(lst) == 9
+
+
+# Test the filter chain. Possible scenarios: 56.
+# Only a subset is selected
+def test_filter_chain_files_only_takes_precedence():
+    lst = exec_filter_chain(FILES_TO_FILTER, True, True, True)
+    assert len(lst) == 3
+
+
+def test_filter_chain_files_all_by_default():
+    sz = len(FILES_TO_FILTER)
+    lst = exec_filter_chain(FILES_TO_FILTER)
+    assert len(lst) == sz
+
+
+def test_filter_chain_files_all_explicit():
+    sz = len(FILES_TO_FILTER)
+    lst = exec_filter_chain(FILES_TO_FILTER, False, False, False)
+    assert len(lst) == sz
+
+
+def test_filter_chain_files_all_explit_keyword():
+    sz = len(FILES_TO_FILTER)
+    lst = exec_filter_chain(FILES_TO_FILTER, exclude_parents=False,
+                            files_only=False, folders_only=False)
+    assert len(lst) == sz
+
+
+def test_filter_chain_files_all_explit_keyword_mixed():
+    sz = len(FILES_TO_FILTER)
+    lst = exec_filter_chain(FILES_TO_FILTER, False,
+                            files_only=False, folders_only=False)
+    assert len(lst) == sz
+
+
+def test_filter_chain_files_only():
+    lst = exec_filter_chain(FILES_TO_FILTER, files_only=True)
+    assert len(lst) == 3
+
+
+def test_filter_chain_files_only_no_keyword():
+    lst = exec_filter_chain(FILES_TO_FILTER, False, True)
+    assert len(lst) == 3
+
+
+def test_filter_chain_folders_only():
+    lst = exec_filter_chain(FILES_TO_FILTER, folders_only=True)
+    assert len(lst) == 9
+
+
+def test_filter_chain_folders_only_no_keyword():
+    lst = exec_filter_chain(FILES_TO_FILTER, False, False, True)
+    assert len(lst) == 9
+
+
+def test_filter_chain_folders_only_no_parents():
+    lst = exec_filter_chain(FILES_TO_FILTER, folders_only=True, exclude_parents=True)
+    assert len(lst) == 7
+
+
+def test_filter_chain_folders_only_no_parents_no_keywords():
+    lst = exec_filter_chain(FILES_TO_FILTER, True, False, True)
+    assert len(lst) == 7
