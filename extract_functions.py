e55d4d7f675d5d779fe4acf17dafa62a31034baa
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-02-08 17:04:41 +0100

    Changed to accept attribs without owner info

--- extract_functions.py
+++ extract_functions.py
@@ -1,49 +1,27 @@
-import os
-import re
 from typing import Callable
 
 
-# possible system domains
-NTAUTH_DOMAIN = 'NT AUTHORITY'
-BUILTIN_DOMAIN = 'BUILTIN'
-BUILTIN_ADMIN = 'BUILTIN\Administrators'
-NTSERV_DOMAIN = 'NT SERVICE'
-LOCAL_DOMAIN = os.environ.get('userdomain')
+BUILTIN_ADMIN = r'BUILTIN\Administrators'
 SPECIAL_DOMAIN = '...'
-all_domains = [NTAUTH_DOMAIN, BUILTIN_DOMAIN,
-               NTSERV_DOMAIN, LOCAL_DOMAIN, SPECIAL_DOMAIN]
-
-
-class MissingOwner(Exception):
-    pass
-
 
 ExtractFunction = Callable[[str], tuple[str]]
-
-
-def get_valid_domain_from(name: str) -> str | None:
-    checked = [dom for dom in all_domains if name.find(dom) >= 0]
-    if checked:
-        return checked[0]
-
-    return None
 
 
 def extract_attribs_cmd(lin: str) -> tuple[str]:
     '''extract attributes at fixed starting positions in the line.
        expects output from console from CMD.exe
     '''
-    dom = get_valid_domain_from(lin)
-    if not dom:
-        raise MissingOwner(
-            'Could not find valid owner field, did you use "/Q"')
     datestr = lin[0:12].strip()
     timestr = lin[12:17].strip()
     sizestr = lin[17:36].strip().replace(',', '')
     owner = lin[36:59].strip()
-    filename = lin[59:].strip()
-    if owner.find('...') > 0:
-        owner = BUILTIN_ADMIN    # Probably, user account cannot know
+    if owner.find('\\') > 0:
+        filename = lin[59:].strip()
+        if owner.find(SPECIAL_DOMAIN) > 0:
+            owner = BUILTIN_ADMIN    # Probably, user account cannot know
+    else:
+        owner = '-'
+        filename = lin[36:]
 
     return datestr, timestr, sizestr, owner, filename
 
@@ -51,21 +29,24 @@ def extract_attribs_cmd(lin: str) -> tuple[str]:
 def extract_attribs(lin: str) -> tuple[str]:
     '''extract attributes at fixed starting positions in the line
     '''
-    dom = get_valid_domain_from(lin)
-    if not dom:
-        raise MissingOwner(
-            'Could not find valid owner field, did you use "/Q"')
     datestr = lin[0:12].strip()
     timestr = lin[12:17].strip()
     sizestr = lin[17:35].strip().replace(',', '')
     own_name = lin[35:]
-    if own_name.find('...') == -1:
-        pat = rf'({dom}\\.*)\b\s{{2}}(.*)'
-        res = re.search(pat, lin).groups()
-        owner, filename = res
-    else:
+    if own_name.startswith(SPECIAL_DOMAIN):
+        # special case of resticted access to file info
         owner = BUILTIN_ADMIN    # Probably, user account cannot know
         filename = own_name[3:].strip()
+
+    if pos := own_name.find('\\') > 0:
+        # there is owner info
+        dom = own_name[0:pos]
+        parts = own_name[pos+1:].split()
+        owner = '\\'.join([dom, parts[0]])
+        filename = ' '.join(parts[1:])
+    else:
+        owner = '-'
+        filename = own_name
 
     return datestr, timestr, sizestr, owner, filename
 
